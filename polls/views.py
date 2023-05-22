02505ec0f39e7c0e806652a1253240141c6854cd
sobak333N <enchantressimba@gmail.com>
2023-05-22 23:03:17 +0300

    com3

--- polls/views.py
+++ polls/views.py
@@ -10,5 +10,5 @@ def index(request):
       b = cursor.fetchall()
 
       a = '<a>'+str(b[0][0])+'</a>'+'<p>'+str(b[0][1])+'</p>'+'<a>'+str(b[0][2])+'</a>'+'<p>'+str(b[0][3])+'</p>'
-
-      return render(request, 'polls/first.html',{'data':a})
+      c = '<a>'+str(b[1][0])+'</a>'+'<p>'+str(b[1][1])+'</p>'+'<a>'+str(b[1][2])+'</a>'+'<p>'+str(b[1][3])+'</p>'
+      return render(request, 'polls/first.html',{'data':a} , {'sdata':c})
