9961bebc966f71933d70d0bd0b1b719d7d95d2a6
sobak333N <enchantressimba@gmail.com>
2023-05-23 00:23:48 +0300

    com4

--- polls/views.py
+++ polls/views.py
@@ -2,13 +2,30 @@ from django.shortcuts import render
 import psycopg2
 def index(request):
       video_id1 = 1
+      i = 0
       connection = psycopg2.connect(host='localhost', dbname='youtube',
-                                    user='postgres', password='3107asdzxc')
+                                   user='postgres', password='3107asdzxc')
       cursor = connection.cursor()
-      request = f"select * from youtube where id = {video_id1}"
       cursor.execute(request)
       b = cursor.fetchall()
+      a = '''
+                  <img src="{% static 'polls/pictures/line.png' %}">
+                  <div id="pic", align="left">
+                <img src="{% static 'polls/pictures/h''' + str(b[i][0])+ '''.jpg' %}">
+            </div>
+            <div id="text" >
+                <a> rang ''' +str(b[i][0])+ ''' </a><p> '''+str(b[i][1])+ ''' </p><a> '''+str(b[i][2])+ ''' </a><p>'''+str(b[i][3])+'''</p></div>'''
 
-      a = '<a>'+str(b[0][0])+'</a>'+'<p>'+str(b[0][1])+'</p>'+'<a>'+str(b[0][2])+'</a>'+'<p>'+str(b[0][3])+'</p>'
-      c = '<a>'+str(b[1][0])+'</a>'+'<p>'+str(b[1][1])+'</p>'+'<a>'+str(b[1][2])+'</a>'+'<p>'+str(b[1][3])+'</p>'
-      return render(request, 'polls/first.html',{'data':a} , {'sdata':c})
+      while i < 90 :
+            a = a + '''
+                  <img src="{% static 'polls/pictures/line.png' %}">
+            <div id="pic", align="left">
+                <img src="{% static 'polls/pictures/h''' + str(b[i][0])+ '''.jpg' %}">
+            </div>
+            <div id="text" >
+                <a> rang ''' +str(b[i][0])+ ''' </a><p> '''+str(b[i][1])+ ''' </p><a> '''+str(b[i][2])+ ''' </a><p>'''+str(b[i][3])+'''</p>
+            </div>
+            '''
+            i +=1
+
+      return render(request, 'polls/first.html',{'data':a})
